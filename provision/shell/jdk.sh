68dd23cfebe181f4cd04eb7ae90b75d8aac45a0e
siniida <sinpukyu@gmail.com>
2014-11-18 17:47:03 +0900

    gennai 2014/11/15版に対応
    * JDK1.7対応
    * initスクリプト改修
     * root以外での実行を不可
     * 多重起動チェック
    * README.md修正

--- provision/shell/jdk.sh
+++ provision/shell/jdk.sh
@@ -4,18 +4,17 @@ USER=vagrant
 
 echo "in jdk."
 
-if [ -d /usr/java/jdk1.6.0_45 ] ; then
+if [ -d /usr/java/jdk1.7.0_71 ] ; then
 	echo " - already."
 	exit 0
 fi
 
 cd /tmp
 echo " - download."
-curl -L -O https://s3-ap-northeast-1.amazonaws.com/gennai/binary/jdk-6u45-linux-x64-rpm.bin >/dev/null 2>&1
+curl -L -O https://s3-ap-northeast-1.amazonaws.com/gennai/binary/jdk-7u71-linux-x64.rpm >/dev/null 2>&1
 
 echo " - install."
-chmod +x ./jdk-6u45-linux-x64-rpm.bin
-./jdk-6u45-linux-x64-rpm.bin >/dev/null 2>&1
+rpm -ivh ./jdk-7u71-linux-x64.rpm >/dev/null 2>&1
 
 # environment
 OUTPUT=/home/${USER}/.bashrc
@@ -24,8 +23,6 @@ echo "export JAVA_HOME=/usr/java/default" >> ${OUTPUT}
 echo "export PATH=\${JAVA_HOME}/bin:\${PATH}" >> ${OUTPUT}
 
 # cleanup
-rm /tmp/jdk-6u45-linux-x64-rpm.bin
-rm /tmp/jdk-6u45-linux-amd64.rpm
-rm sun-javadb-*.rpm
+rm /tmp/jdk-7u71-linux-x64.rpm
 
 #EOF
