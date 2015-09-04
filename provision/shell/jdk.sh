0b2339a3c0b349194c21f6908653fedff2beaa01
siniida <sinpukyu@gmail.com>
2015-09-04 20:21:03 +0900

    0814版にアップデート

--- provision/shell/jdk.sh
+++ provision/shell/jdk.sh
@@ -11,10 +11,10 @@ fi
 
 cd /tmp
 echo " - download."
-curl -L -O https://s3-ap-northeast-1.amazonaws.com/gennai/binary/jdk-7u71-linux-x64.rpm >/dev/null 2>&1
+curl -L -O -b "oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/7u80-b15/jdk-7u80-linux-x64.rpm >/dev/null 2>&1
 
 echo " - install."
-rpm -ivh ./jdk-7u71-linux-x64.rpm >/dev/null 2>&1
+rpm -ivh ./jdk-7u80-linux-x64.rpm >/dev/null 2>&1
 ln -s /usr/java/default/bin/jps /usr/bin/jps
 
 # environment
@@ -24,6 +24,6 @@ echo "export JAVA_HOME=/usr/java/default" >> ${OUTPUT}
 echo "export PATH=\${JAVA_HOME}/bin:\${PATH}" >> ${OUTPUT}
 
 # cleanup
-rm /tmp/jdk-7u71-linux-x64.rpm
+rm /tmp/jdk-7u80-linux-x64.rpm
 
 #EOF
